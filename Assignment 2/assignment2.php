be0c24764ee1758b4014ae0a1f732724519f280c
J-MaFf <7maffiolajoey@gmail.com>
2023-02-09 21:50:56 -0600

    Activity 1 completed after messing around with errounious code for hours. Sheesh.

--- Assignment 2/assignment2.php
+++ Assignment 2/assignment2.php
@@ -3,7 +3,7 @@
     $courseSectionRegEx = "/(\w*[A-Z]+\s?\t?\d{2,3}[A-Z]?)\s(\d{2})/";
 
     // Modify the regular expression in Part 1 to capture all the scheduled course sections with titles.
-    $courseTitleSectionRegEx = "/(\w*[A-Z]+\s?\t?\d{2,3}[A-Z]?)\s(\d{2})(?>\s\d+\s)([A-Z\s]+[A-Z]{3,})/"
+    $courseTitleSectionRegEx = "/(\w*[A-Z]+\s?\t?\d{2,3}[A-Z]?)\s(\d{2})(?>\s\d+\s)([A-Z\s]+[A-Z]{3,})/";
 
     // Modify the regular expression in Part 2 to capture all the scheduled course sections with titles, components, and credits.
     $courseTitleSectionComponentCreditsRegEx = "/(\w*[A-Z]+\s?\t?\d{2,3}[A-Z]?)\s(\d{2})(?>\s\d+\s)([A-Z\s]+[A-Z]{3,})\s([A-z\s]+)\s+(\d\s-\s\d)/";
@@ -12,4 +12,4 @@
     $courseTitleSectionComponentCreditsInstructorRegEx = "/(\w*[A-Z]+\s?\t?\d{2,3}[A-Z]?)\s(\d{2})(?>\s\d+\s)([A-Z\s]+[A-Z]{3,})\s([A-z\s]+)\s+(\d\s-\s\d)(?>\s+.+:\s)([A-z,]+\s[A-z]+)/";
 
     
-/?>
+?>
