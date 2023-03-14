760c813e866eec9ac742edb1180281c51ee6f4e8
Joey M <7maffiolajoey@gmail.com>
2023-03-14 15:55:12 -0500

    Setting up my Laravel environment

---
+++ sample-app/resources/views/greeting.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Greeting</title>
+</head>
+
+<body>
+    <h2>Hello, <?php echo $name; ?></h2>
+    <h3>Welcome to <?php echo $course; ?></h3>
+</body>
+
+</html>
--- sample-app/routes/web.php
+++ sample-app/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('default');
 });
+// How to add a new route
+//          URL         Controller
+Route::get('/welcome', [WelcomeController::class, 'index']);
