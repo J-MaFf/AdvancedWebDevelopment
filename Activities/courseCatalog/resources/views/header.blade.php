0c4f672aa3eda5d86b096cf22437a08a2ef3684e
Joey M <7maffiolajoey@gmail.com>
2023-03-16 15:55:25 -0500

    started in class activity

---
+++ Activities/courseCatalog/resources/views/header.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Schedule of Classes</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+   <style>
+    
+    .navbar .navbar-nav  a {
+    color: white;
+}
+    </style>
+
+</head>
+
+<body class="antialiased">
+<nav class="navbar navbar-expand-lg " style="background-color: purple; color: white;">
+        <div class="container-fluid">
+            
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                   <a class="nav-link" aria-current="page" href={{url("/")}}>Home</a>
+                   <a class="nav-link" aria-current="page" href={{url("/coursesbysubject/COMPSCI")}}>Course Catalog</a>
+                </div>
+            </div>
+        </div>
+    </nav>
