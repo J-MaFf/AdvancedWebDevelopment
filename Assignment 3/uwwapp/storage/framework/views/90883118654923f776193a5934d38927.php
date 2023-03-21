171ff5bd0d9627b7765463f87f921c0313cf5d23
Joey M <7maffiolajoey@gmail.com>
2023-03-21 15:48:10 -0500

    Activities

---
+++ Assignment 3/uwwapp/storage/framework/views/90883118654923f776193a5934d38927.php
@@ -0,0 +1,46 @@
+<?php echo $__env->make('includes.header', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+<body>
+    <?php echo $__env->make('includes.navbar', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <h3>Course Catalog</h3>
+                <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                        Select Subject
+                    </button>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <li><a class="dropdown-item" href="<?php echo e(url('/coursesbysubject')); ?>">All</a></li>
+                        <?php $__currentLoopData = $subjects; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $s): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                            <li><a class="dropdown-item" href="<?php echo e(url('/coursesbysubject', [$s->subject])); ?>"><?php echo e($s->full_name); ?></a></li>
+                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                    </ul>
+                </div>
+                <?php if(isset($subject)): ?>
+                    <h2>List of <?php echo e($subject); ?> Courses</h2>
+                <?php else: ?>
+                    <h2>List of All Courses</h2>
+                <?php endif; ?>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Course</th>
+                            <th>Title</th>
+                            <th>Credits</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php $__currentLoopData = $courses; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $c): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                            <tr>
+                                <td><?php echo e($c->subject); ?> <?php echo e($c->number); ?></td>
+                                <td><?php echo e($c->title); ?></td>
+                                <td><?php echo e($c->credits); ?></td>
+                            </tr>
+                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</body><?php /**PATH C:\Users\7maff\Jetta\XAMPP\htdocs\AdvancedWebDevelopment\Assignment 3\uwwapp\resources\views/allsubjects.blade.php ENDPATH**/ ?>
---
+++ Assignment 3/uwwapp/storage/framework/views/dd95ebdae9fd020f18852ea001af02e1.php
@@ -0,0 +1,12 @@
+<?php echo $__env->make('includes.header', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+<body>
+    <?php echo $__env->make('includes.navbar', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+    <!-- Homepage image -->
+    <img src="https://www.uww.edu/images/admissions/tour/campus-tour-main1.jpg" class="img-fluid" alt="Campus img">
+    
+</body>
+
+</html>
+<?php /**PATH C:\Users\7maff\Jetta\XAMPP\htdocs\AdvancedWebDevelopment\Assignment 3\uwwapp\resources\views/uww.blade.php ENDPATH**/ ?>
