2e99ba1f3f91ac5d307a6e3e5416fc38e2fa6421
Joey M <7maffiolajoey@gmail.com>
2023-05-09 19:54:01 -0500

    Creating a navbar

--- Final Project/MyPortfolio/src/app/app.module.ts
+++ Final Project/MyPortfolio/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, NavbarComponent],
   imports: [BrowserModule, AppRoutingModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent],
